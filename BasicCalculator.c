92d85296b4dfba42a04a017bb8d1c51bcb99833d
orny-code <lutfunnesaorny@gmail.com>
2026-05-27 14:30:08 +0600

    Add files via upload

---
+++ BasicCalculator.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+int main()
+{
+    float num1,num2,result;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f",&num1);
+
+    printf("Enter second number: ");
+    scanf("%f",&num2);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    switch(op)
+    {
+    case'+':
+        result = num1+num2;
+        printf("Result = %.2f\n",result);
+        break;
+
+    case'-':
+        result = num1-num2;
+        printf("Result = %.2f\n",result);
+        break;
+
+    case '*':
+        result =  num1*num2;
+        printf("Result = %.2f\n",result);
+        break;
+
+    case '/':
+        if(num2!=0)
+        {
+            result = num1/num2;
+            printf("Result = %.2f\n",result);
+        }
+        else
+        {
+            printf("Error! Division by zero is undefined.\n");
+        }
+        break;
+
+    default:
+        printf("Invalid operator!\n");
+
+    }
+}
